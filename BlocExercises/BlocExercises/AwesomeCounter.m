788e24bed8dd63f8ce067d3226b16c4107b469fa
Kevin Colligan <kevincolligan@gmail.com>
2015-03-05 13:36:31 -0800

    Take two on loops - Completed with a single While loop

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,42 +12,37 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     
+    // CREATE VARIABLES
+    
+    NSInteger lowNumber;
+    NSInteger highNumber;
     NSMutableString *expectedString;
     
-    /* WHILE LOOP FOR WHEN NUMBER < OTHER NUMBER */
     
-    if (number < otherNumber) {
-        
-        expectedString = [NSMutableString stringWithFormat:@"%ld", (long)number];
-        while (number < otherNumber) {
-            NSInteger current = ++number;
-            NSString *currentNumber = [NSString stringWithFormat:@"%ld", (long)current];
-            [expectedString appendString:currentNumber];
-        }
-        
-    } else if (number > otherNumber) {
-        
-         /* I was going to try a For Loop here, but I couldn't figure out */
-        
-        expectedString = [NSMutableString stringWithFormat:@"%ld", (long)otherNumber];
-        while (otherNumber < number) {
-            NSInteger current = ++otherNumber;
-            NSString *currentNumber = [NSString stringWithFormat:@"%ld", (long)current];
-            [expectedString appendString:currentNumber];
-        }
-        
-    } else if (number == otherNumber) {
-        
-         /* JUST GIVE BACK NUMBER IF = OTHER NUMBER LOOP FOR WHEN NUMBER < OTHER NUMBER */
-        
-        expectedString = [NSMutableString stringWithFormat:@"%ld", (long)number];
-        
+    // SET LOW & HIGH NUMBERS
+    
+    if (number <= otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
     }
+    
+    
+    // SINGLE WHILE LOOP
+    
+    expectedString = [NSMutableString stringWithFormat:@"%ld", (long)lowNumber];
 
-  
+    while (lowNumber < highNumber) {
+            NSInteger current = ++lowNumber;
+            NSString *currentNumber = [NSString stringWithFormat:@"%ld", (long)current];
+            [expectedString appendString:currentNumber];
+        }
+        
+    
 
     return expectedString;
-    
     
 }
 
