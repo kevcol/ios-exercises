681fbc8e89bdb98a0e0f2ca204cde65aee0b39c9
Kevin Colligan <kevincolligan@gmail.com>
2015-03-07 15:13:46 -0800

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,18 +11,34 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *drinkUp = [characterDictionary valueForKey:@"favorite drink"];
+    
+    return drinkUp;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+    
+    /* BRANDON: This seemed too easy -- should I be running a loop here?  I put this in first and was surprised it passed.  Then I printed out array to NSLog and it listed both drinks, so I suppose it is good, eh?  */
+     
+    NSArray *lastCall = [charactersArray valueForKey:@"favorite drink"];
+    
+    return lastCall;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
+    
+    // make a mutable copy
+    NSMutableDictionary *quotedTrek = [characterDictionary mutableCopy];
+    
+    // Find missing quote
+    if (!quotedTrek[@"quote"]) {
+        
+        // add silly quote
+        [quotedTrek setObject:@"Does this phaser make me look fat?" forKey:@"quote"];
+    }
+    
+    return quotedTrek;
 }
 
 @end
